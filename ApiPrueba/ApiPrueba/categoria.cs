c8a21f2827d59ed72fd17f67bacaf53da90dc9a2
Julian David Fierro Casanova <julifierrocasanova@gmail.com>
2024-07-26 12:34:06 -0500

    c++

---
+++ ApiPrueba/ApiPrueba/categoria.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiPrueba
+{
+    internal class categoria
+    {
+        private int idCategoria { get; set; }
+        private string nombreCategoria { get; set; }
+        private string descripcionCategoria { get; set; }
+
+        public categoria(int idCategoria, string nombreCategoria, string descripcionCategoria)
+        {
+            this.idCategoria = idCategoria;
+            this.nombreCategoria = nombreCategoria;
+            this.descripcionCategoria = descripcionCategoria;
+        }
+
+        //Hace que devuelva nombre y descripcion
+        public override string ToString()
+        {
+            return $"Categoria: {nombreCategoria}, Descripción: {descripcionCategoria}";
+        }
+    }
+}
---
+++ ApiPrueba/ApiPrueba/producto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiPrueba
+{
+    internal class producto
+    {
+        private int idProducto { get; set; }
+        private string nombreProducto { get; set; }
+        private decimal precioProducto { get; set; }
+        private string descripcionProducto { get; set; }
+
+        // Relación con la categoría
+        private categoria categoria { get; set; } 
+
+        public producto(int idProducto, string nombreProducto, decimal precioProducto, string descripcionProducto, categoria categoria)
+        {
+            this.idProducto = idProducto;
+            this.nombreProducto = nombreProducto;
+            this.precioProducto = precioProducto;
+            this.descripcionProducto = descripcionProducto;
+            this.categoria = categoria;
+        }
+
+        public override string ToString()
+        {
+            return $"Producto: {nombreProducto}, Descripción: {descripcionProducto}, Categoría: {categoria}";
+        }
+    }
+}
+
+
